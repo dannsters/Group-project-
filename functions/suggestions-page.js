bd797fb5985d4fb8cace1c804f94280f6c14ba66
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-04-06 23:49:53 -0500

    Create suggestions-page.js

---
+++ functions/suggestions-page.js
@@ -0,0 +1,9 @@
+'use strict'
+const mysql = require('mysql2');
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+const serverless = require('serverless-http');
+const router = express.Router();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
