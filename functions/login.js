ab92720996a5049c97ec737971213d3c6dc5b46d
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-04-07 01:03:06 -0500

    Update login.js

--- functions/login.js
+++ functions/login.js
@@ -1,1 +1,23 @@
+'use strict'
+const mysql = require('mysql2');
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+const serverless = require('serverless-http');
+const router = express.Router();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+
+
+app.use('/',(req,res)=>{
+
+  
+
+
+  
+})
+
+app.use('/login',router)
+module.exports.handler = serverless(app)
 
