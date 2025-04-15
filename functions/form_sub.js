1fef92f7b2ec2ac69543d4c7e34d26e20e5140db
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-04-15 10:13:27 -0500

    Create form_sub.js

---
+++ functions/form_sub.js
@@ -0,0 +1,49 @@
+dise = req.body
+var substance_abuse = {start:dise.alc_drug_start,
+                    end:dise.alc_drug_end,
+                    comments:dise.alc_drug_comments  
+                      }
+var asthma = {start:dise.asthma_start,
+                    end:dise.asthma_end,
+                    comments:dise.asthma_comments  
+                      }
+var cancer = {start:dise.cancer_start,
+                    end:dise.cancer_end,
+                    comments:dise.cancer_comments  
+                      }
+var depressives = {start:dise.dep_anx_start,
+                    end:dise.dep_anx_end,
+                    comments:dise.dep_anx_comments  
+                      }
+var diabetes = {start:dise.diab_start,
+                    end:dise.diab_end,
+                    comments:dise.diab_comments  
+                      }
+var copd = {start:dise.copd_start,
+                    end:dise.copd_end,
+                    comments:dise.copd_comments  
+                      }
+var heart_disease = {start:dise.heart_d_start,
+                    end:dise.heart_d_end,
+                    comments:dise.heart_d_comments  
+                      }
+var highbloodpressure = {start:dise.HBP_start,
+                    end:dise.HBP_end,
+                    comments:dise.HBP_comments  
+                      }
+var highcholesterol = {start:dise.hcholesterol_start,
+                    end:dise.hcholesterol_end,
+                    comments:dise.hcholesterol_comments  
+                      }
+var thyroiddisease = {start:dise.thy_d_start,
+                    end:dise.thy_d_end,
+                    comments:dise.thy_d_comments  
+                      }
+var kidneydisease = {start:dise.ren_d_start,
+                    end:dise.ren_d_end,
+                    comments:dise.ren_d_comments  
+                      }
+var migraine = {start:dise.migraine_start,
+                    end:dise.migraine_end,
+                    comments:dise.migraine_comments  
+                      }
