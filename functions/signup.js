0853bc6385ccc89be316d366e75cd73c5269269e
coloneljp3 <113227613+coloneljp3@users.noreply.github.com>
2025-04-23 02:36:08 -0500

    Update signup.js

--- functions/signup.js
+++ functions/signup.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use('/',(req,res)=>{
 var customer = req.body
 var username = customer.username
-var password = customer.lastname
+var password = customer.pasword
 
 var connect = mysql.createConnection(process.env.DATABASE_URL)
 
@@ -21,12 +21,265 @@ var connect = mysql.createConnection(process.env.DATABASE_URL)
 if(results.length==0){connect.query('INSERT INTO Customers VALUES(?,?)',[username,password],(err,results)=>{
 res.send(`
 
-Some File
-`)
+<header id="main-header" style="height:600px;background-color:#0b4ecc"><div style="z-index:-1;background-color: #35e8fe;"><div style="z-index:1;position:absolute;top:50%;background-color:transparent;margin:auto;width:100%;height:100px"><div style="font-weight:bold;font-family:Raleway;opacity:100%;color:white;background-color:transparent"><p style="font-family:'Varela Round';top:-450px;font-size:120px;position:absolute;left:100px;width:50%"><br>The<br>Medicator</p><p style="position:absolute;right:20px;width:50%">The premier healthcare app in managing your prescriptions, writing new ones, and staying safe on medication<br></p></div></div><img style="opacity:60%;width:100%;height:100%;" src="https://harcourthealth.com/wp-content/uploads/2019/10/technology-in-healthcare.jpg"></div>
+<div style="background-color:transparent;height:50%;width:100%">
+
+<div id="main-nav-bar-top-half">
+<br>
+
+<script>
+window.onload = (event) =>{
+document.getElementById('search').style.width = '350px';
+
+}
+
+
+window.onclick = (event) =>{
+document.getElementById('options').innerHTML = '';
+var menu = document.getElementById('menu-bar') 
+menu.style.width = "0px";
+menu.style.height = "0px";
+menu.style.fontSize = "0px"
+var specialOffers = document.getElementById('special-offers');specialOffers.style.visibility = hidden;
+}
+  window.onload = (event) =>{
+document.getElementById('search').style.width = '350px';
+}
+window.onclick = (event) =>{
+document.getElementById('options').innerHTML = '';
+}
+    
+var search_list = {
+
+
+        }
+function searchReq(){
+  document.getElementById('options').innerHTML = '';
+var val = document.getElementById('search').value;
+var list = Object.keys(search_list);
+
+for(let i of list){
+
+if(i.includes(val.toUpperCase())){
+    var elem = document.createElement('div');
+    var a = document.createElement('a');
+    var but = document.createElement('button')
+    
+    a.className = 'search_op';
+    elem.className = 'search_op';
+but.style.fontFamily = 'Raleway'
+but.style.color = "white";
+but.style.borderColor = "rgb(145 151 81)"
+but.style.backgroundColor = "rgb(145 151 81)";
+but.style.borderRadius = "10px"
+but.style.fontWeight = "bold";
+but.style.margin = "0px";
+but.style.display = "block"
+but.style.height = "50px";
+but.style.width= "275px";
+a.style.position = "absolute"
+    elem.style.visibility = "visible";
+    a.style.visibility = "visible";
+    elem.style.height = "50px";
+    a.style.height = "50px" 
+    a.href = list[i];
+	a.append(but)
+    but.append(i);
+
+    elem.appendChild(a);
+    document.getElementById('options').append(elem);
+
+
+}
+
+
+
+}
+
+}
+</script>
+<style>
+:hover:-moz-placeholder{
+    color:rgba(0, 0, 120, 0.8)
+    
+  } 
+  
+  :hover::-webkit-input-placeholder { 
+    color:rgba(0, 0, 120, 0.8)}
+  
+  :hover:-ms-input-placeholder { 
+    color:rgba(0, 0, 120, 0.8)}
+  
+  :hover::-moz-placeholder{ 
+    color:rgba(0, 0, 120, 0.8)}
+  
+  
+  
+  
+a{
+text-decoration:none
+}
+
+	#options{position:absolute;z-index:1;width:50%;margin:auto;}
+#search{
+border-radius:25px;
+font-family:Cormorant Garamond;
+width:0px;
+border-style:solid;
+border-color:#a3b8c8;
+display:inline-block;
+height:50px;
+text-align:center;
+border-color:white;
+transition:width 1s;
+border-style:solid}
+</style>
+  
+
+<div style=" width: 120px;position:fixed;top:10px;left:50%"><svg id="search_icon" onclick="
+
+var width = document.getElementById('search');
+if(width.style.width == '0px'){
+width.style.width = '120px';
+}
+else{
+width.style.width = '0px'}
+
+
+" style="z-index:1;width:30px;height:20px;">
+<circle cx="8" cy="8" stroke-width="3" stroke="black" r="6"></circle>
+<polyline points="12 12 20 20 " stroke-width="3" stroke="black" fill="none"></polyline>
+</svg><input placeholder="Find Out More About Us" onkeyup="" id="search" type="search" style="width: 0px;float:left"></div>
+
+<div style="position:relative"> 
+    
+<div id="options"></div>
+<div id="precursor"></div>
+</div><br>
+</div><div><svg style="height:50px;width:40px;z-index:1;position:fixed;left:15px;top:10px" onmouseover="menuStat('menu-bar')">
+
+<polyline stroke="whitesmoke" stroke-width="4" points="0 10 40 10"></polyline>
+<polyline stroke="whitesmoke" stroke-width="4" points="0 20 40 20"></polyline>
+<polyline stroke="whitesmoke" stroke-width="4" points="0 30 40 30"></polyline>
+
+</svg><div id="menu-bar" onmouseleave="menuStat('menu-bar')" style="width: 0px; height: 0px; font-size: 0px; top: 0%;">
+<a href="" class="menu-bar-options"><button type="submit" class="menu-bar-options">About Us</button></a>
+<a href="" class="menu-bar-options"><button type="submit" class="menu-bar-options">Drug Information</button></a>
+<a href="" class="menu-bar-options"><button type="submit" class="menu-bar-options">Careers</button></a>
+<a href="" class="menu-bar-options"><button type="submit" class="menu-bar-options">Specials</button></a>
+<a href="" class="menu-bar-options"><button type="submit" class="menu-bar-options">Create Account/Login</button></a>
+
+
+</div></div>
+
+
+ 
+
+<nav id="main-nav-bar">
+
+<br>
+
+<div style="position:fixed;top:10px;right:50px">
+
+
+
+
+
+
+
+
+
+
+
+
+<span class="main-nav-bar-items-container" onmouseleave="revDropDown('dropdown-button-6')" style="background-color:transparent">
+
+<form action="" method="POST" class="main-nav-bar-form" style="background-color:transparent">
+<input name="username" value = `+user+` style="display:none">
+<input name="pasword" value = `+password+` style="display:none">
+<button onmouseover="" type="submit" id="home-button" class="main-nav-bar-form-button" style="background-color:transparent;font-family:Helvetica;font-weight:lighter;color:whitesmoke">Medical Forms</button>
+
+</form>
+<div class="main-nav-bar-dropdown-container">
+<div class="main-nav-bar-dropdown">
+
+
+
+
+
+</div>
+
+</div>
+</span>
+
+
+
+
+<span class="main-nav-bar-items-container" onmouseleave="revDropDown('dropdown-button-7')" style="background-color:transparent">
+
+<form action="" method="POST" class="main-nav-bar-form" style="background-color:transparent">
+<input name="username" value = `+user+` style="display:none">
+<input name="pasword" value = `+password+` style="display:none">
+<button onmouseover="" type="submit" id="home-button" class="main-nav-bar-form-button" style="background-color:transparent;font-family:Helvetica;font-weight:lighter;color:whitesmoke">Accounts</button>
+
+</form>
+<div class="main-nav-bar-dropdown-container">
+<div class="main-nav-bar-dropdown">
+
+
+
+
+
+</div>
+
+</div>
+</span><span class="main-nav-bar-items-container" onmouseleave="revDropDown('dropdown-button-8')" style="background-color:transparent">
+
+<form action="" method="POST" class="main-nav-bar-form" style="background-color:transparent">
+<input name="username" value = `+user+` style="display:none">
+<input name="pasword" value = `+password+` style="display:none">
+<button onmouseover="" type="submit" id="home-button" class="main-nav-bar-form-button" style="background-color:transparent;font-family:Helvetica;font-weight:lighter;color:whitesmoke">APIs</button>
+
+</form>
+<div class="main-nav-bar-dropdown-container" style="background-color:transparent">
+<div class="main-nav-bar-dropdown">
+
+
+
+
+
+</div>
+
+</div>
+</span><span class="main-nav-bar-items-container" onmouseleave="revDropDown('dropdown-button-9')" style="background-color:transparent">
+
+<form action="" method="POST" class="main-nav-bar-form" style="background-color:transparent">
+<input name="username" value = `+user+` style="display:none">
+<input name="pasword" value = `+password+` style="display:none">
+<button onmouseover="" type="submit" id="home-button" class="main-nav-bar-form-button" style="background-color:transparent;font-family:Helvetica;font-weight:lighter;color:whitesmoke">About</button>
+
+</form>
+<div style="background-color:transparent" class="main-nav-bar-dropdown-container">
+<div class="main-nav-bar-dropdown">
+
+
+
+
+
+</div>
+
+</div>
+</span></div>
+
+
+
+</nav>
+
+</div></header>`)
 
   
 })}
-else(throw err; console.log('This account already exists'))
+else(res.send('This account already exists'))
     
   })
   
